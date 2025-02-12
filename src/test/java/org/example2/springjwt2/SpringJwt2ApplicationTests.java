0ea842dccde4e6a9534f0d6a90cb6ce80fc2f3ae
muddie <parkjm0817@gmail.com>
2025-02-12 20:15:55 +0900

    first commit

---
+++ src/test/java/org/example2/springjwt2/SpringJwt2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example2.springjwt2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJwt2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
